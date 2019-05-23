1793aabfd4032f277c5e41129dc4f98d1150a817
Tim W <timothy.woodbury@swri.org>
2019-05-23 17:30:06 -0500

    Adding vehicle class definition to world header

--- include/simple_sim/world.h
+++ include/simple_sim/world.h
@@ -1,10 +1,19 @@
 #ifndef __WORLD_H_INCLUDED__
 #define __WORLD_H_INCLUDED__
 
+#include <vector>
+
 class world{
 public:
   world();
   double x;
+private:
+  vector <vehicle> trackedVehicles; //!< list of vehicles that are known to this object
 };
+
+/*! A generic vehicle class used by a world object for truth histories */
+class vehicle{
+
+}
 
 #endif
